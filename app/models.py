63c73d1d15827e19086aa7e56b20a15599bf1c6f
Sharvani Rao <sm3676@njit.edu>
2026-04-14 16:23:20 -0400

    Fixed models.py datetime and duplicates

--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
 from sqlalchemy import Column, Integer, String, DateTime
-from datetime import datetime
+from datetime import datetime, UTC
 from app.database import Base
+
 
 class User(Base):
     __tablename__ = "users"
@@ -9,4 +10,4 @@ class User(Base):
     username = Column(String, unique=True, index=True, nullable=False)
     email = Column(String, unique=True, index=True, nullable=False)
     password_hash = Column(String, nullable=False)
-    created_at = Column(DateTime, default=datetime.utcnow)
+    created_at = Column(DateTime, default=lambda: datetime.now(UTC))
--- app/schemas.py
+++ app/schemas.py
@@ -1,14 +1,16 @@
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, EmailStr, ConfigDict
 
+# 🔹 For creating user (REQUEST)
 class UserCreate(BaseModel):
     username: str
     email: EmailStr
     password: str
 
+
+# 🔹 For returning user (RESPONSE)
 class UserRead(BaseModel):
     id: int
     username: str
     email: EmailStr
 
-    class Config:
-        from_attributes = True
+    model_config = ConfigDict(from_attributes=True)
